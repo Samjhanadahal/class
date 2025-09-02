d136ab46994d4dceade5d71fc9fef3adeaecd51f
samjhana <dahalsamjhana770@gmail.com>
2025-09-02 10:51:31 +0545

    folder added

---
+++ Typesofvariables.java
@@ -0,0 +1,18 @@
+
+public class Typesofvariables {
+int gender;
+
+
+void display (){
+    int name; 
+    int age ; 
+    System.out.println ("name"  +num1 +"age" +age);
+}
+     public static void main(String[] args) {
+        Typesofvariables  typesofvariables = new Typesofvariables();
+          
+        typesofvariables.display();
+
+     }
+ 
+}
